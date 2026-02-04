d5044a484058dd59caf19c02e0dba9f2e26c8e01
Srijan kumar <Srijan kumar>
2026-02-04 16:45:11 +0530

    last attempt to get the smtp error fixed

--- backend/src/lib/mailer.js
+++ backend/src/lib/mailer.js
@@ -6,12 +6,14 @@ dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // STARTTLS
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  logger: true, // Log to console
+  debug: true, // Include debug info
 });
 
 // verify on startup
